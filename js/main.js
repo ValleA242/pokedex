26586d1f51dc730c07276b9571f1aa7bff3ab1fc
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-06-18 08:41:58 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,17 +8,17 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        let pokeName = data.forms[0].name.charAt(0).toUpperCase() + data.forms[0].name.slice(1);
-        let pokeType = data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1);
+        let pokeName = data.forms[0].name.charAt(0).toUpperCase() + data.forms[0].name.slice(1); //assign pokemon name to variable 
+        let pokeType = data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1); // assign pokemon type to variable 
 
 
         
         
-        document.querySelector('#name-screen').innerText = pokeName
-        document.querySelector('#type-screen').innerText = pokeType
-        document.querySelector('#weight').innerText = `Weight: ${Number(data.weight)/10}kg`
-        document.querySelector('#height').innerText = `Height: ${data.height / 10}m`
-        document.querySelector('#main-screen').style.backgroundImage = `url(${data.sprites.other.home.front_default})`
+        document.querySelector('#name-screen').innerText = pokeName //display pokemon name in pokedex 
+        document.querySelector('#type-screen').innerText = pokeType //display pokemon type in pokedex 
+        document.querySelector('#weight').innerText = `Weight: ${Number(data.weight)/10}kg` //display pokemon weight in pokedex 
+        document.querySelector('#height').innerText = `Height: ${data.height / 10}m` // display pokemon height in pokedex 
+        document.querySelector('#main-screen').style.backgroundImage = `url(${data.sprites.other.home.front_default})` //
         document.querySelector('#id-screen').innerText = `#${data.id}`
         console.log(data.sprites.other.home.front_default)
       })
